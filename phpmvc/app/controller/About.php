3a6862da4b1dceac2dbcd1313d5397cd1bcfe84b
dwiandikka06 <164830852+dwiandikka06@users.noreply.github.com>
2024-03-27 09:01:22 +0700

    Add files via upload

---
+++ phpmvc/app/controller/About.php
@@ -0,0 +1,13 @@
+<?php 
+ 
+class About{
+    
+    public function index($nama = 'anis', $pekerjaan = 'buruh')
+    {
+        echo "halo saya $nama    manusia $pekerjaan";
+    } 
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
